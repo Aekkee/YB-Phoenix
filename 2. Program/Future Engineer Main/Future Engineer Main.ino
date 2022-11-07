6dc6a73ba7adb525879aad1d541203d1e21cb7ea
natapol2547 <96534097+natapol2547@users.noreply.github.com>
2022-11-07 23:16:30 +0700

    Bug fixes

--- 2. Program/Future Engineer Main/Future Engineer Main.ino
+++ 2. Program/Future Engineer Main/Future Engineer Main.ino
@@ -7,6 +7,8 @@
 /*-//-//-//-//-//-//-//-//-//- LIBRARY SETUP -//-//-//-//-//-//-//-//-//-*/
 
 // PID
+// #include <PID_v2.h>
+// PID_v2 myPID(1, 0, 1, PID::Direct);
 float err, last_err, integral;
 long PIDtimer;
 
@@ -18,13 +20,14 @@ int USread;
 
 // Cytron Maker Driver Library
 #include "CytronMotorDriver.h"
-CytronMD motor1(PWM_PWM, 3, 4);
+CytronMD motor1(PWM_PWM, 2, 3);
 
 // Compass
 #include <Wire.h>
 #define ADDRESS 0x60
 byte highByte;
 byte lowByte;
+bool Reading = false;
 int bearing = 0;
 long Ctimer;
 int initial_deg;
@@ -43,13 +46,21 @@ Servo myservo2;  // Steering
 */
 
 void setup() {
+  // myPID.Start(0,              // input
+  //             0,              // current output
+  //             0);             // setpoint
   pinMode(26, INPUT_PULLUP);  // Set START button pin mode
   myservo1.attach(13, 600, 2400);
   myservo2.attach(12, 600, 2400);
-
-  while (digitalRead(26) == 1)  // Wait until START button is pressed
+  CompassUpdate();
+  while (digitalRead(26) == 0)  // Wait until START button is pressed
     ;
   initial_deg = bearing;  // Set initial robot direction
+  // CompassUpdate();
+
+  // while (digitalRead(26) == 1)  // Wait until START button is pressed
+  //   ;
+  // initial_deg = bearing;  // Set initial robot direction
 }
 
 /* setup1() and loop1()
@@ -61,12 +72,30 @@ void setup1() {
   Wire1.setSCL(7);  // Set SCL pin for I2C communication
   Wire1.begin();
   Serial.begin(115200);  // Begin Serial communication for debugging
+
+  Wire1.beginTransmission(ADDRESS);  //starts communication with cmps03
+  Wire1.write(12);
+  Wire1.write(0x55);
+  Wire1.endTransmission();
+  Wire1.beginTransmission(ADDRESS);  //starts communication with cmps03
+  Wire1.write(13);
+  Wire1.write(0x5A);
+  Wire1.endTransmission();
+  Wire1.beginTransmission(ADDRESS);  //starts communication with cmps03
+  Wire1.write(14);
+  Wire1.write(0xA5);
+  Wire1.endTransmission();
+  Wire1.beginTransmission(ADDRESS);  //starts communication with cmps03
+  Wire1.write(15);
+  Wire1.write(0x12);
+  Wire1.endTransmission();
 }
 
 
 void loop() {
-
-  motor1.setSpeed(255);
+  digitalWrite(2, 1);
+  digitalWrite(3, 0);
+  analogWrite(8, 4095);
 
   // int pos;
   // for (pos = 0; pos <= 270; pos += 1) {  // goes from 0 degrees to 180 degrees
@@ -89,8 +118,15 @@ void loop() {
   // }
   // float wrap = Wrap((bearing - initial_deg) * -1, -180, 179);
   // float test = PID(wrap, 0.5, 0, 3);
+  // Serial.println(Wrap((bearing - initial_deg) * -1, -180, 179));
+  // int why = myPID.Run(Wrap((bearing - initial_deg) * -1, -180, 179)) + 135;
+  // Servo2(why);
 
-  Servo2(PID(Wrap((bearing - initial_deg) * -1, -180, 179), 0.5, 0, 3) + 135);  // Set steering degree
+  // Servo2(PID(Wrap((bearing - initial_deg) * -1, -180, 179), 0.7, 0, 2000) + 135);  // Set steering degree
+
+  Servo2(Wrap((PID(bearing - initial_deg, 1, 0, 5)) * -1, -180, 179) * 0.5 + 135);
+
+  // Servo2(Wrap((bearing - initial_deg) * -1, -180, 179) + 135);
 
   // Serial.print(wrap);
   // Serial.print(" --> ");
@@ -103,25 +139,27 @@ void loop() {
   //   // initial_deg = initial_deg + 360;
   //   delay(10);
   // }
+  // Serial.println(Wrap((PID(bearing - initial_deg, 1, 0, 5)) * 1, -180, 179) + 135);
+  // Serial.println(Wrap((bearing - initial_deg) * -1 + 135, -180, 179));
 }
 
 void loop1() {
 
   CompassUpdate();
-  UltraSonicUpdate();
+  // UltraSonicUpdate();
 
 
   // Serial.println(bearing);
 }
 
-void Servo1(long theta) {
+void Servo1(int theta) {
   theta = constrain(theta, 45, 225);   // Limit Servo degree
   theta = map(theta, 0, 270, 0, 180);  // Map degree to correct the position
   // Serial.println(pos);
   myservo1.write(theta);
 }
 
-void Servo2(long theta) {
+void Servo2(int theta) {
   theta = constrain(theta, 90, 180);   // Limit servo degree
   theta = map(theta, 0, 270, 0, 180);  // Map degree to correct the position
   // Serial.println(pos);
@@ -136,27 +174,34 @@ void UltraSonicUpdate() {
 }
 
 void CompassUpdate() {
-  if (millis() - Ctimer > 50) {        // Update Ultrasonic every 50 ms
+  if (Reading == false) {
     Wire1.beginTransmission(ADDRESS);  //starts communication with cmps03
     Wire1.write(2);                    //Sends the register we wish to read
     Wire1.endTransmission();
+    Reading = true;
+    Ctimer = millis();
+    // Serial.println("Beginning Transimission");
+  }
+  if (millis() - Ctimer > 33) {  // Update Ultrasonic every 50 ms
 
     Wire1.requestFrom(ADDRESS, 2);    //requests high byte
     while (Wire1.available() < 2) {}  //while there is a byte to receive
     highByte = Wire1.read();          //reads the byte as an integer
     lowByte = Wire1.read();
-    bearing = ((highByte << 8) + lowByte) / 10;   // Convert to degree
-    Ctimer = millis();
+    bearing = ((highByte << 8) + lowByte) / 10;  // Convert to degree
+    Reading = false;
+    // Serial.println(millis() - Ctimer);
   }
 }
 
 float PID(float input, float kp, float ki, float kd) {
-  if (millis() - PIDtimer > 10) {
+  if (millis() - PIDtimer > 2) {
     err = input * kp + integral * ki + (input - last_err) * kd;
     integral = integral + err;
     last_err = input;
     PIDtimer = millis();
   }
+  // Serial.println(err);
   return err;
 }
 
--- 2. Program/Perfected_Line_Detection_Program/Perfected_Line_Detection_Program.ino
+++ 2. Program/Perfected_Line_Detection_Program/Perfected_Line_Detection_Program.ino
@@ -11,20 +11,13 @@
 
 /*-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-*/
 
+
+
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
 
-/* Example code for the Adafruit TCS34725 breakout library */
-
-/* Connect SCL to analog 5
-  Connect SDA to analog 4
-  Connect VDD to 3.3V DC
-  Connect GROUND to common ground */
 
 /* Initialise with default values (int time = 2.4ms, gain = 1x) */
-// Adafruit_TCS34725 tcs = Adafruit_TCS34725();
-
-/* Initialise with specific int time and gain values */
 Adafruit_TCS34725 tcs = Adafruit_TCS34725();
 
 void setup(void) {
